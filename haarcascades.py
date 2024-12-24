34b6f8351349fb279121e07e84f35dc9d16c3441
scev <minh.nn210582@sis.hust.edu.vn>
2024-12-24 13:19:09 +0700

    Refactor

--- haarcascades.py
+++ haarcascades.py
@@ -1,14 +1,20 @@
 import cv2
 import os
 
-def process_image(image):
+def haarcascades_process(image):
     if not os.path.exists("output"):
         os.makedirs("output")
+    
+    face_cascade = cv2.CascadeClassifier('models/haarcascade_frontalface_default.xml')
+    
     output_image = cv2.resize(image, (640, 480))
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     gray = cv2.cvtColor(output_image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+    print(faces)
+    
     for (x, y, w, h) in faces:
         cv2.rectangle(output_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     return output_image
+
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 from file_helper import get_image, write_image
-from process_face import process_image
+from haarcascades import haarcascades_process
 
 def main():
     try:
         image, input_image_path = get_image()
         print(f"Image loaded: {input_image_path}")
 
-        output_image = process_image(image)
+        output_image = haarcascades_process(image)
         output_image_path = write_image(output_image)    
         print(f"Processed image saved to {output_image_path}")
     
