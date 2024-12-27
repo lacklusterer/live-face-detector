f4f7bfe45fff20cce50d0a9d7894f4ac390cfbc3
scev <minh.nn210582@sis.hust.edu.vn>
2024-12-28 01:31:56 +0700

    fixed yunet_annote

--- models/yunet/yunet_annote.py
+++ models/yunet/yunet_annote.py
@@ -4,11 +4,13 @@ def get_annote(image, debug=False):
     height, width = image.shape[:2]
     face_detector = cv2.FaceDetectorYN_create('models/yunet/face_detection_yunet_2023mar.onnx', "", (width, height))
     _, faces = face_detector.detect(image)
-    
-    annote = []
-    if faces is not None:
-        annote = faces
-        if debug:
-            print(annote)
+    if debug:
+        print(faces)
+    return faces
 
-    return annote
+def get_bounding_boxes(image, debug=False):
+    faces = get_annote(image, debug)
+    bounding_boxes = [face[:4] for face in faces]
+    if debug:
+        print(f"Bounding Boxes: {bounding_boxes}")
+    return bounding_boxes
